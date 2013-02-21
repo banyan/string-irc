46d621a00f83ee3a7dc35f27e59e60f1e4c64f6b
Kohei Hasegawa <ameutau@gmail.com>
2013-02-21 19:04:32 +1100

    cosmetic: fix indent

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'rspec'
 require 'string-irc'
 
@@ -8,5 +9,4 @@ require 'string-irc'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
 end
