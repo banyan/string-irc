1ab510a410a00fcde0c762dc0d684ac40e74ed2d
Kohei Hasegawa <ameutau@gmail.com>
2013-02-23 19:13:43 +1100

    feature improvement: add original color scheme

--- lib/string-irc.rb
+++ lib/string-irc.rb
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 class StringIrc
-
   COLOR_CODE = "\x03" # \u0003
   BOLD       = "\x02" # \u0002
   UNDERLINE  = "\x1f" # \u001F
@@ -27,24 +26,47 @@ class StringIrc
     15 => %w(light_grey silver),
   }
 
+  ORIGINAL_COLOR_SCHEME = {
+    "rainbow"      => %w(red orange yellow green blue light_blue purple), # order is referred: http://ja.wikipedia.org/wiki/%E8%99%B9
+    "seven_eleven" => %w(orange green red)
+  }
+
   def initialize(string = nil)
     @string = string
   end
 
-  color_name_table = {}
+  @@color_name_table = {}
 
-  COLOR_TABLE.each do | code, colors |
+  COLOR_TABLE.each do |code, colors|
     colors.each do |color|
-      color_name_table[color] = code
-      define_method(color) do | *args |
+      @@color_name_table[color] = code
+      define_method(color) do |*args|
         bg_color = args.first || nil
-        if (!!bg_color and color_name_table.include?(bg_color))
-          color_code = "#{COLOR_CODE}#{sprintf("%02d,%02d", code, color_name_table[bg_color])}"
+        raise ArgumentError, "`#{bg_color}' is not allowed to use as background color" if ORIGINAL_COLOR_SCHEME.include?(bg_color)
+        if bg_color && @@color_name_table.include?(bg_color)
+          color_code = "#{COLOR_CODE}#{"%02d,%02d" % [code, @@color_name_table[bg_color]]}"
         else
-          color_code = "#{COLOR_CODE}#{sprintf("%02d", code)}"
+          color_code = "#{COLOR_CODE}#{"%02d" % code}"
         end
         add_code_l(color_code)
       end
+    end
+  end
+
+  ORIGINAL_COLOR_SCHEME.each do |key, order|
+    define_method(key) do |*args|
+      index    = 0
+      bg_color = args.first || nil
+      @string = @string.each_char.map { |char|
+        order = index % ORIGINAL_COLOR_SCHEME[key].size
+        index += 1
+        if bg_color && @@color_name_table.include?(bg_color)
+          "#{COLOR_CODE}#{"%02d,%02d" % [@@color_name_table[ORIGINAL_COLOR_SCHEME[key][order]], @@color_name_table[bg_color]]}#{char}"
+        else
+          "#{COLOR_CODE}#{"%02d" % @@color_name_table[ORIGINAL_COLOR_SCHEME[key][order]]}#{char}"
+        end
+      }.join
+      self
     end
   end
 
--- spec/string-irc_spec.rb
+++ spec/string-irc_spec.rb
@@ -1,72 +1,58 @@
 # coding: utf-8
-
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
 describe StringIrc do
-  share_examples_for 'when it first created' do
-    specify { should be_an_instance_of StringIrc }
-  end
+  subject { StringIrc.new('hello') }
 
-  context 'with an argument "hello"' do
-    subject { StringIrc.new('hello') }
-
-    it_should_behave_like 'when it first created' do
-      its(:to_s) { should == "hello\x0f" }
-    end
-
-    context "single color" do
-      its("red.to_s")          { should == "\x0304hello\x0f" }
-      its("blue.to_s")         { should == "\x0302hello\x0f" }
-      its("blue_cyan.to_s")    { should == "\x0310hello\x0f" }
-      its("light_purple.to_s") { should == "\x0313hello\x0f" }
-    end
-
-    context "decoration" do
-      its("bold.to_s")      { should == "\x02hello\x0f" }
-      its("underline.to_s") { should == "\x1fhello\x0f" }
-      its("inverse.to_s")   { should == "\x16hello\x0f" }
-    end
-
-    context "complex" do
-      its("bold.yellow.to_s")                   { should == "\x0308\x02hello\x0f" }
-      its("bold.yellow.underline.to_s")         { should == "\x1f\x0308\x02hello\x0f" }
-      its("bold.yellow.underline.inverse.to_s") { should == "\x16\x1f\x0308\x02hello\x0f" }
-    end
-
-    context "bg-color" do
-      describe 'yellow("red").to_s' do
-        it { subject.yellow("red").to_s.should == "\x0308,04hello\x0f" }
+  describe '#to_s' do
+    context 'when only foreground color passed in' do
+      StringIrc::COLOR_TABLE.each do |color, codes|
+        codes.each do |code|
+          context "foreground color: #{code}" do
+            it { expect(subject.send(code).to_s).to eq("\x03#{ "%02d" % color }hello\x0f") }
+          end
+        end
       end
-      describe 'red("yellow").to_s' do
-        it { subject.red("yellow").to_s.should == "\x0304,08hello\x0f" }
+    end
+
+    context 'when foreground color and background color are passed in' do
+      StringIrc::COLOR_TABLE.each do |color, codes|
+        codes.each do |code|
+          context "foreground color and background color: #{code}" do
+            it { expect(subject.send(code, code).to_s).to eq("\x03#{ "%02d" % color },#{ "%02d" % color }hello\x0f") }
+          end
+        end
       end
-      describe 'red("cyan").to_s' do
-        it { subject.red("cyan").to_s.should == "\x0304,11hello\x0f" }
-      end
+    end
+
+    context 'when colors are not passed in' do
+      it { expect(subject.to_s).to eq("hello\x0f") }
     end
   end
 
-  context "without an argument" do
-    subject { StringIrc.new }
-
-    it_should_behave_like "when it first created" do
-      its(:to_s) { should == "\x0f" }
+  context "when decorating method passed in" do
+    describe '#bold' do
+      it { expect(subject.bold.to_s).to eq("\x02hello\x0f") }
     end
 
-    context "single color" do
-      its("red.to_s") { should == "\x0304\x0f" }
+    describe '#underline' do
+      it { expect(subject.underline.to_s).to eq("\x1fhello\x0f") }
     end
 
-    context "decoration" do
-      its("bold.to_s")      { should == "\x02\x0f" }
-      its("underline.to_s") { should == "\x1f\x0f" }
-      its("inverse.to_s")   { should == "\x16\x0f" }
+    describe '#inverse' do
+      it { expect(subject.inverse.to_s).to eq("\x16hello\x0f") }
+    end
+  end
+
+  context "when original color scheme method passed in" do
+    describe '#rainbow' do
+      it { expect(subject.rainbow.to_s).to eq("\u000304h\u000307e\u000308l\u000303l\u000302o\u000F") }
+      it { expect(subject.rainbow("black").to_s).to eq("\u000304,01h\u000307,01e\u000308,01l\u000303,01l\u000302,01o\u000F") }
     end
 
-    context "complex" do
-      its("bold.yellow.to_s")                   { should == "\x0308\x02\x0f" }
-      its("bold.yellow.underline.to_s")         { should == "\x1f\x0308\x02\x0f" }
-      its("bold.yellow.underline.inverse.to_s") { should == "\x16\x1f\x0308\x02\x0f" }
+    describe '#seven_eleven' do
+      it { expect(subject.seven_eleven.to_s).to eq("\u000307h\u000303e\u000304l\u000307l\u000303o\u000F") }
+      it { expect(subject.seven_eleven("black").to_s).to eq("\u000307,01h\u000303,01e\u000304,01l\u000307,01l\u000303,01o\u000F") }
     end
   end
 end
